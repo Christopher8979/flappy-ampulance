5e67d9b7d7e5dd97280e555f7f26b4020c6aa60c
Christopher Gosala <cgosala@deloitte.com>
2017-10-05 17:30:24 +0530

    Jumbling options everytime we open the application

--- modules/questions/services/index.js
+++ modules/questions/services/index.js
@@ -3,9 +3,23 @@ const UTILS = require('../../../utils.js');
 const ORM = require('../../../ORM');
 
 const jumbleOptions = (questions) => {
+    let placHolder = '';
+    let options = ['a__c', 'b__c', 'c__c', 'd__c'];
+    let jumbledOptions = UTILS.shuffleArray(JSON.parse(JSON.stringify(options)));
+    let tempOptions = [];
 
     async.each(questions, (ques, callBack) => {
-        ques.options = UTILS.shuffleArray(ques.options);
+        tempOptions = [];
+        jumbledOptions.forEach((key, index) => {
+            tempOptions.push(ques[key]);
+        });
+
+        options.forEach((key, index) => {
+            ques[key] = tempOptions[index];
+        });
+
+
+        callBack(null);
     }, (err) => {
         if (err) {
             console.log('Error in shuffling options');
@@ -14,6 +28,7 @@ const jumbleOptions = (questions) => {
     });
 
     return questions;
+
 };
 
 module.exports = {
@@ -26,8 +41,8 @@ module.exports = {
                 return callBack(err, null);
             }
 
-            callBack(null, questions);
+            callBack(null, jumbleOptions(questions));
         });
-                
+
     }
 };
