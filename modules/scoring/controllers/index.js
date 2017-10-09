672602003caaafb03039ad1f587840a1e98e0c68
Christopher Gosala <cgosala@deloitte.com>
2017-10-09 14:39:50 +0530

    Added scoring module

---
+++ modules/scoring/controllers/index.js
@@ -0,0 +1,14 @@
+/**
+ * Controller to interact with other modules or UI for qnything related to scoring.
+ */
+const service = require('../services');
+const config = require('../config.json');
+
+
+module.exports = {
+    getLeaderBoard : () => {},
+    getHighScorrer : () => {},
+    createAttempt : () => {},
+    updateAttempt : () => {},
+    getLatestAttempts : () => {}
+}
---
+++ modules/scoring/services/index.js
@@ -0,0 +1,11 @@
+const async = require('async');
+const UTILS = require('../../../utils.js');
+const ORM = require('../../../ORM');
+
+module.exports = {
+    fetchScores : () => {},
+    fetchTopScorrers : () => {},
+    createAttempt : () => {},
+    saveAttempt : () => {},
+    createPlayer : () => {}
+};
