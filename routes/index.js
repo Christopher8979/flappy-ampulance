9c4dc111b5abd6bddf6e08cab615bcd6ab0c27cb
Christopher Gosala <cgosala@deloitte.com>
2017-10-13 00:29:29 +0530

    Added all basic routes to the application.

--- routes/index.js
+++ routes/index.js
@@ -2,55 +2,88 @@ var express = require('express');
 var router = express.Router();
 
 const MODULES = {
-  questions: require('../modules/questions/controllers')
+    questions: require('../modules/questions/controllers')
 };
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Flappy Ambulance', description: 'Some random text' });
 });
 
-router.get('/questions', (req, res) => {
-  MODULES.questions.getQuestions((err, questions) => {
-    if (err) {
-      return res.status(500).jsonp({
-        status: 'error while fetching questions',
-        error: err
-      });
-    }
+router.post('/checkUser', (req, res) => {
 
-    res.status(200).jsonp(questions);
-  });
+});
+
+router.get('/rules/:id', (req, res) => {
+
+});
+
+router.get('/play-game/:id', (req, res) => {
+
 });
 
 router.post('/check-answer/:attempt/:id', (req, res) => {
 
-  if (!(req.params && req.params.attempt && req.params.id)) {
-    return res.render('error', 'Proper params are not provided');
-  }
+});
 
-  const questionNo = req.params.id;
-  const answeredAs = req.body.answeredAs;
-  const attemptID = req.params.attempt;
+router.get('/game-over/:id', (req, res) => {
 
-  MODULES.questions.checkAnswer(questionNo, answeredAs, (err, response) => {
-    if (err) {
-      console.info('Error wihile checking answers');
-      console.log(err);
-      return res.status(400).jsonp(err);
+});
+
+router.post('/saveAttempt/:id', (req, res) => {
+
+});
+
+router.get('/contributors', (req, res) => {
+
+});
+
+
+router.get('/questions', (req, res) => {
+    MODULES.questions.getQuestions((err, questions) => {
+        if (err) {
+            return res.status(500).jsonp({
+                status: 'error while fetching questions',
+                error: err
+            });
+        }
+
+        res.status(200).jsonp(questions);
+    });
+});
+
+router.post('/check-answer/:attempt/:id', (req, res) => {
+
+    if (!(req.params && req.params.attempt && req.params.id)) {
+        return res.render('error', 'Proper params are not provided');
     }
 
-    res.status(200).jsonp(response);
-  });
+    const questionNo = req.params.id;
+    const answeredAs = req.body.answeredAs;
+    const attemptID = req.params.attempt;
+
+    MODULES.questions.checkAnswer(questionNo, answeredAs, (err, response) => {
+        if (err) {
+            console.info('Error wihile checking answers');
+            console.log(err);
+            return res.status(400).jsonp(err);
+        }
+
+        res.status(200).jsonp(response);
+    });
 });
 
 router.get('/play-game/:id', function (req, res) {
-  if (!(req.params && req.params.id)) {
-    return res.render('400', 'No params in rules page');
-  }
-  res.render('game', {
-    title: 'Playing game now'
-  });
+    if (!(req.params && req.params.id)) {
+        return res.render('400', 'No params in rules page');
+    }
+    res.render('game', {
+        title: 'Playing game now'
+    });
+});
+
+router.get('/*', function (req, res) {
+    res.redirect('/');
 });
 
 module.exports = router;
